1b3388b11360fc417e32407322ab43503ad8a2f8
Abdul Mohaimin <abdul.mohaimin@thebeansgroup.com>
2022-04-12 11:08:32 +0100

    Added ability to retrieve images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,19 +1,31 @@
 import React from "react";
 import "./Meme.css"
+import memesData from "../memesData";
 
 export default function Meme(){
+
+    const [memeUrl,setMemeUrl] = React.useState("")
+
+
+    function getMemeImage(){
+
+        const length = memesData.data.memes.length
+        const randomIndex = Math.floor(Math.random() * length)
+        const randomMeme = memesData.data.memes[randomIndex]
+        setMemeUrl(randomMeme.url)
+    } 
+ 
     return (
         <div className="Meme">
-            <form action="" className="meme-input">
+            <div className="meme-input">
                 <div className="input-fields">
                     <input type="text" className="top-text" placeholder="Top text"/>
                     <input type="text" className="bottom-text" placeholder="Bottome text"/>
                 </div>
                 
-                <input type="submit" className="submit-button" value="Get a new image 🌁"/>
-
-                
-            </form>
+                <input type="submit" className="submit-button" value="Get a new image 🌁" onClick = {getMemeImage}/>
+            </div>
+            <img src={memeUrl} alt="Meme" className="meme-image  " />
         </div>
     );
 }
